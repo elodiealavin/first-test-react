0fbbe17d2f007b3277d6d2e4719f005c20c5da6b
Elodie <elodie@air-de-elodie.lan>
2023-12-06 15:52:50 +0100

    finish test

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,26 @@ import "./App.css";
 import data from "./assets/data.json";
 
 function App() {
-  const [movies, setMovies] = useState({});
+  const [movies, setMovies] = useState();
   // console.log("console1", movies);
   // console.log("console4", actors); //undifed
   return (
     <div>
-      <section className="container">
-        {data.map((elem, id) => {
-          console.log("console2", elem);
-          console.log("console3", id);
-          return (
-            <main>
-              <div className="button">
+      <main
+        className="container"
+        style={{
+          backgroundImage: movies ? `url(${movies.background})` : "none",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <section>
+          <header className="button">
+            {data.map((elem, id) => {
+              console.log("console2", elem);
+              console.log("console3", id);
+              return (
                 <button
                   key={elem.id}
                   onClick={() => {
@@ -23,29 +31,35 @@ function App() {
                 >
                   <p>{elem.name}</p>
                 </button>
+              );
+            })}
+          </header>
+        </section>
+        <div className="home">
+          {movies === undefined ? (
+            <span>Merci de choisir un film !</span>
+          ) : (
+            <div className="card-container">
+              <div className="title">
+                <h1>{movies.name}</h1>
               </div>
-
-              {movies.id === elem.id && (
-                <div className="main-container">
-                  {elem.actors.map((actor, index) => {
-                    console.log("console5", actor);
-                    return (
-                      <div className="card-actor" key={index}>
-                        <div className="picture">
-                          {/* <img src={actor.picture} /> */}
-                        </div>
-                        <div className="name">
-                          {/* <p className="name-actor">{actor.name}</p> */}
-                        </div>
-                      </div>
-                    );
-                  })}
-                </div>
-              )}
-            </main>
-          );
-        })}
-      </section>
+              {movies.actors.map((actor, index) => {
+                console.log("console5", actor);
+                return (
+                  <div className="card-actor" key={index}>
+                    <div className="picture">
+                      <img src={actor.picture} />
+                    </div>
+                    <div className="name">
+                      <p>{actor.name}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </main>
     </div>
   );
 }
